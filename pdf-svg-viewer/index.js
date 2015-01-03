e680601ec5eac8ceff28b4349cd3213997819a9d
Adam Saunders <saunders@cs.umass.edu>
2015-01-03 11:17:40 -0500

    wip

---
+++ pdf-svg-viewer/index.js
@@ -0,0 +1,24 @@
+console.log('hey there');
+//  function mousemove() {
+//  }
+// 
+// 
+//  var svg = d3.select('#ccc')
+//        .insert('svg')
+//        .attr('width', 1000)
+//        .attr('height', 1000);
+// 
+// 
+// 
+// 
+// 
+//fs.readdir("../data/0801.3042.pdf.d/", function cb (err, files){
+//  _.foreach(files, function f(file){
+//    console.log('sdjfk ');
+//  });
+//});
+//// "data/0801.3042.pdf.d/0801.3043-7.svg"
+// 
+// 
+// 
+// 
